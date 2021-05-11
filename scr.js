9607213fb008e3417aa93369e581622e1ba4d605
Sundaraviswanath <82366172+Sundaraviswanath@users.noreply.github.com>
2021-05-11 10:05:06 +0530

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    let num = document.getElementById("number").innerHTML = 10;
+    setTimeout(() => {
+        let num = document.getElementById("number").innerHTML = 9;
+        setTimeout(() => {
+            let num = document.getElementById("number").innerHTML = 8;
+            setTimeout(() => {
+                let num = document.getElementById("number").innerHTML = 7;
+                setTimeout(() => {
+                    let num = document.getElementById("number").innerHTML = 6;
+                    setTimeout(() => {
+                        let num = document.getElementById("number").innerHTML = 5;
+                        setTimeout(() => {
+                            let num = document.getElementById("number").innerHTML = 4;
+                            setTimeout(() => {
+                                let num = document.getElementById("number").innerHTML = 3;
+                                setTimeout(() => {
+                                    let num = document.getElementById("number").innerHTML = 2;
+                                    setTimeout(() => {
+                                        let num = document.getElementById("number").innerHTML = 1;
+                                        setTimeout(() => {
+                                            let num = document.getElementById("number").innerHTML = "Welcome to Coding";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
